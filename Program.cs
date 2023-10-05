7ff9d3b6ac37dbf024686ecec1588d042e1a2411
alexi <alexi@LAPTOP-K1LI30UL>
2023-10-04 23:40:07 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace problema3semana7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Años bisiestos desde 1900:");
+
+            for (int año = 1900; año <= 2023; año++)
+            {
+                // Verificar si el año es bisiesto
+                if ((año % 4 == 0 && año % 100 != 0) || (año % 400 == 0))
+                {
+                    Console.Write("Es un año bisiesto:\t"); Console.WriteLine(año);
+                }
+            }Console.ReadKey();
+        }
+    }
+}
